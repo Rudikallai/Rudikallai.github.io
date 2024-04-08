7c04e09b1f96836bc8ee7172f3c8bccaeed0483d
Rudikallai <rudolf4.kallai>
2024-04-08 20:51:47 +0200

    .

--- my-pages/2/js/index.js
+++ my-pages/2/js/index.js
@@ -13,3 +13,19 @@ hamElement.addEventListener("click", function () {
     loginElement.classList.toggle("font4");
 });
 
+let mybutton = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
