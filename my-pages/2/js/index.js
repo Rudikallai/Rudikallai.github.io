a068215db723d08167f3367a7be6d3c080ca4dd2
Rudikallai <rudolf4.kallai>
2024-04-14 21:09:51 +0200

    .

--- my-pages/2/js/index.js
+++ my-pages/2/js/index.js
@@ -55,3 +55,39 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+
+
+
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function prevSlide() {
+    showSlide(slideIndex -= 1);
+}
+
+function nextSlide() {
+    showSlide(slideIndex += 1);
+}
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel img');
+    const texts = [
+        "The 918 Spyder",
+        "The GT3 RS",
+        "The 911 Turbo S"
+    ];
+    const carouselText = document.querySelector('.carousel-text p');
+
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slides[slideIndex].style.display = 'block';
+    carouselText.textContent = texts[slideIndex];
+}
