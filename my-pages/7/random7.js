c4491c03d80a71287eb59ad4c3d170544d5e45ed
Rudikallai <rudolf4.kallai>
2024-04-17 22:12:47 +0200

    .

---
+++ my-pages/7/random7.js
@@ -0,0 +1,26 @@
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  document.getElementById("generateButton").addEventListener("click", function() {
+    var minNumber = 1;
+    var maxNumber = 35;
+    var generatedNumbers = [];
+  
+    var container = document.getElementById("randomNumberContainer");
+    container.innerHTML = ""; // Clear previous numbers
+  
+    // Generate 5 different random numbers
+    for (var i = 0; i < 7; i++) {
+      var randomNumber;
+      do {
+        randomNumber = generateRandomNumber(minNumber, maxNumber);
+      } while (generatedNumbers.includes(randomNumber)); // Check if the number already exists
+      generatedNumbers.push(randomNumber);
+  
+      var randomNumberElement = document.createElement("span");
+      randomNumberElement.textContent = randomNumber;
+      randomNumberElement.classList.add("randomNumber");
+      container.appendChild(randomNumberElement);
+    }
+  });
